ebe1d1ce6369849a12db311622a242eae92a00a8
Kleo <mmttdd16@gmail.com>
2023-03-22 05:03:45 +0300

    Finished Ex3

--- Ex3/Program.cs
+++ Ex3/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int dailOfWeek = Convert.ToInt32(Console.ReadLine());
+if (dailOfWeek==1)
+{
+    System.Console.WriteLine("Monday");
+}
+else if (dailOfWeek==2)
+{
+    System.Console.WriteLine("Tuesday");
+}
+else if (dailOfWeek==3)
+{
+    System.Console.WriteLine("Wednesday");
+}
+else if (dailOfWeek==4)
+{
+    System.Console.WriteLine("Thursday");
+}
+else if (dailOfWeek==5)
+{
+    System.Console.WriteLine("Friday");
+}
+else if (dailOfWeek==6)
+{
+    System.Console.WriteLine("Saturday");
+}
+else if (dailOfWeek==7)
+{
+    System.Console.WriteLine("Sunday");
+}
+else 
+{
+    System.Console.WriteLine("Eror");
+}
+
