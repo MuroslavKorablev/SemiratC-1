013f7887aec8229729ba03fb8ff7b02d40bfb4fc
Kleo <mmttdd16@gmail.com>
2023-03-22 04:31:03 +0300

    Finished Ex1 finaly

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -2,6 +2,7 @@
 int inputNumber = Convert.ToInt32(Console.ReadLine());
 //Convert.ToInt32(Console.ReadLine());
 // int inputNumber = Convert.Parse(Console.ReadLine()); нужно будет еще поправить в csproj Nullable вкл на Disable
-int square = inputNumber * inputNumber;
+//int square = inputNumber * inputNumber;
+int square = Convert.ToInt32(Math.Pow(inputNumber, 10)) ;
 
 Console.WriteLine(square);
