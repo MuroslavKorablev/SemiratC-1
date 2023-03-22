cd3a1a1e3abcf8c110830193bf5b05cbac33fa40
Kleo <mmttdd16@gmail.com>
2023-03-22 05:41:06 +0300

    Finished Ex5

--- Ex5/Program.cs
+++ Ex5/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трехзначное число и на выход показывает последнюю цифру этого числа.
+System.Console.WriteLine("Видите трехзначное число ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a / 100;
+System.Console.WriteLine (b);
+int c = (a % 100)/10;
+System.Console.WriteLine(c);
+int d = a % 10;
+System.Console.WriteLine(d);
