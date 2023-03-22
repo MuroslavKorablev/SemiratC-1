3aa9db5320afea66a0a91396d7ecd93fb6f9532a
Kleo <mmttdd16@gmail.com>
2023-03-22 04:24:27 +0300

    Finished Ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿// 1 обЪявить тип переменной, объявляем имя переменной
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+//Convert.ToInt32(Console.ReadLine());
+// int inputNumber = Convert.Parse(Console.ReadLine()); нужно будет еще поправить в csproj Nullable вкл на Disable
+int square = inputNumber * inputNumber;
+
+Console.WriteLine(square);
